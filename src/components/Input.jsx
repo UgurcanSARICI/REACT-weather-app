49a4aa1582e04a1231255a54842e058fd84452ac
UgurcanSARICI <ugurcansarici@gmail.com>
2022-10-01 15:24:27 +0300

    first commit

---
+++ src/components/Input.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Input = () => {
+  return (
+    <div>
+      <div>
+        <input type="" placeholder="Enter a city" className="" />
+      </div>
+    </div>
+  );
+};
+
+export default Input;
